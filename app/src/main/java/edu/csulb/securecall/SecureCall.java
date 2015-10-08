54434db88dae66c8b762e171ac6659b607fa9985
gagandeep7717 <gagandeep7717@gmail.com>
2015-10-07 19:53:16 -0700

    Project Initial Stage
    We have Implemented initial project stage.
    We are currently working on study of android and voip options

---
+++ app/src/main/java/edu/csulb/securecall/SecureCall.java
@@ -0,0 +1,13 @@
+package edu.csulb.securecall;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecureCall extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secure_call);
+    }
+}
